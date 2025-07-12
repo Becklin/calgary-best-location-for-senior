08ab65821d2139822066099bb48f5a192d976142
Beck <beckyenwen@gmail.com>
2025-07-12 10:04:42 -0600

    configure vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ['@arcgis/core']
+  },
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000,
